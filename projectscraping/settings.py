34c49fbcf3a738722cc74e37174a3fcd708d5be7
Daniel Gualteros <dgualteros637@gmail.com>
2023-09-06 14:06:42 -0500

    cambio en url para azure

--- projectscraping/settings.py
+++ projectscraping/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)*x!hz@636gcmtul%hlo+4n6l#=p8y5_jd#9$=#7%zor$)sh5#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['webscrapingapp-323.azurewebsites.net','']
+ALLOWED_HOSTS = ['webscrapingapp-323.azurewebsites.net']
 #ALLOWED_HOSTS = ['scraping-323-c07f0718337b.herokuapp.com','']
 #ALLOWED_HOSTS = []
 CSRF_TRUSTED_ORIGINS = [
